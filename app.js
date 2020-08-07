6d0409dbe365546c17f3c238fa733ffa14ba91e5
samanthajeet <samanthajeet@gmail.com>
2020-08-07 14:25:53 -0600

    incorporated mongodb/mongoose. added get routes

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 6060;
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { hello: 'this is my api' };
+    const query = {};
+    if (req.query.genre) { // validate url query is equal to genre and not something random
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
 
-    res.json(response);
+bookRouter.route('/books/:bookdId')
+  .get((req, res) => {
+    Book.findById(req.params.bookdId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
   });
 
 app.use('/api', bookRouter);
---
+++ bookJson.js
@@ -0,0 +1,44 @@
+db.books.insert([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Tolstoy',
+    read: false,
+  },
+  {
+    title: 'Do Androids Dream of Electric Sheep?',
+    genre: 'Science Fiction',
+    author: 'Philip K. Dick',
+    read: true,
+  },
+  {
+    title: 'Bossypants',
+    genre: 'Autobiography',
+    author: 'Tina Fey',
+    read: true,
+  },
+  {
+    title: 'Shadows of the Workhouse',
+    genre: 'Historical Nonfiction',
+    author: 'Jennifer Worth',
+    read: true,
+  },
+  {
+    title: 'Brave New World',
+    genre: 'Science Fiction',
+    author: 'Aldous Huxley',
+    read: true,
+  },
+  {
+    title: 'Song of Solomon',
+    genre: 'Fiction',
+    author: 'Toni Morrison',
+    read: false,
+  },
+  {
+    title: 'Mere Christianity',
+    genre: 'Religion',
+    author: 'C.S. Lewis',
+    read: false,
+  },
+]);
